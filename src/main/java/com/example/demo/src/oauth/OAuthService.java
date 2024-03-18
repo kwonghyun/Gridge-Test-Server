cef9580d3ac3116026c4722aacbd725cdc8c9058
kwonghyun <momky4@naver.com>
2024-03-18 15:26:01 +0900

    chore: 패키지, 파일 구조 변경 - user, oauth 분리

--- src/main/java/com/example/demo/src/oauth/OAuthService.java
+++ src/main/java/com/example/demo/src/oauth/OAuthService.java
@@ -8,7 +8,6 @@ import com.example.demo.src.oauth.model.KakaoUser;
 import com.example.demo.src.user.entity.User;
 import com.example.demo.src.user.model.GetSocialOAuthRes;
 import com.example.demo.src.user.model.PostUserReq;
-import com.example.demo.src.user.model.PostUserRes;
 import com.example.demo.src.user.service.UserService;
 import com.example.demo.utils.JwtService;
 import com.fasterxml.jackson.core.JsonProcessingException;
