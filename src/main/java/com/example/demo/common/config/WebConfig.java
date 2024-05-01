fb56e5882879ed72048af33bd01b4c5c11a858c0
kwonghyun <momky4@naver.com>
2024-05-01 22:47:42 +0900

    chore: cors 허용

--- src/main/java/com/example/demo/common/config/WebConfig.java
+++ src/main/java/com/example/demo/common/config/WebConfig.java
@@ -12,9 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 앞으로 만들 모든 CORS 정보를 적용한다
-        registry.addMapping("/**") 
-                // Header의 Origin에 들어있는 주소가 http://localhost:3000인 경우를 허용한다
-                .allowedOrigins("http://localhost:8081", "http://localhost:63342", "https://sns.worklog.shop")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:63342", "https://sns.worklog.shop", "http://sns.worklog.shop")
                 // 모든 HTTP Method를 허용한다.
                 .allowedMethods("*")
                 // HTTP 요청의 Header에 어떤 값이든 들어갈 수 있도록 허용한다.
